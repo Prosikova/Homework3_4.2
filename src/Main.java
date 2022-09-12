54623155088b2770ddf98374de8c85671acd1c97
Prosikova <lvenok182@mail.ru>
2022-09-13 02:22:59 +0300

    Выполнение задания 2 в домашней работе 3_4 (переменные 2 часть)

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+        public static void main(String[] args) {
+                double time = 27.12;
+                System.out.println(time);
+                long people = 987678965549L;
+                System.out.println(people);
+                float weight = 2.786f;
+                System.out.println(weight);
+                short potatoes = 569;
+                System.out.println(potatoes);
+                short dutyDriver = -159;
+                System.out.println(dutyDriver);
+                int boxes = 27897;
+                System.out.println(boxes);
+                byte shrubs = 67;
+                System.out.println(shrubs);
+        }
+}
