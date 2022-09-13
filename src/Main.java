7d4f3977d37a80f7ef7bc9f6a06f436f66d40476
Prosikova <lvenok182@mail.ru>
2022-09-13 21:30:34 +0300

    Выполнение задания 2 в домашней работе 3 (переменные 2 часть). Внесены коррективы в решение домашнего задания.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
         public static void main(String[] args) {
+                System.out.println("Задание 2");
                 double time = 27.12;
                 System.out.println(time);
                 long people = 987678965549L;
@@ -7,6 +8,7 @@ public class Main {
                 float weight = 2.786f;
                 System.out.println(weight);
                 short potatoes = 569;
+                boolean variable1 = false;
                 System.out.println(potatoes);
                 short dutyDriver = -159;
                 System.out.println(dutyDriver);
